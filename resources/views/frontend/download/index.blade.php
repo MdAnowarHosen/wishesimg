a508961b08a71eb40109ca7ea1e9f2bc8061d459
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-03-25 08:52:46 +0600

    download page design

--- resources/views/frontend/download/index.blade.php
+++ resources/views/frontend/download/index.blade.php
@@ -9,11 +9,9 @@
                 <div>
                      {{-- Mobile Download button --}}
                     <div class="mt-5 mb-3 block md:hidden">
-                        <a href="#">
-                            <div class=" bg-green-600 rounded-full  text-gray-100 text-center py-1 font-semibold">
+                            <button type="button" data-modal-target="download-modal" data-modal-toggle="download-modal" class=" w-full bg-green-600 rounded-full  text-gray-100 text-center py-1 font-semibold">
                             <x-bi-download width="50" height="20" class="block mx-auto" /> Free Download
-                            </div>
-                        </a>
+                            </button>
                     </div>
                     <img class=" block mx-auto rounded-lg" style="max-height: 600px;"
                      src="https://cdn.pixabay.com/photo/2023/03/18/10/43/plum-blossoms-7860381_960_720.jpg"
@@ -176,10 +174,10 @@
 
               {{-- Download button --}}
               <div class="mt-5">
-                <a href="#">
-                    <div class=" bg-green-600 rounded-lg  text-gray-100 text-center py-1 font-semibold">
+                <div class=" cursor-pointer" data-modal-target="download-modal" data-modal-toggle="download-modal">
+                    <button type="button" class=" bg-green-600 rounded-lg  text-gray-100 text-center py-1 font-semibold w-full">
                      <x-bi-download width="50" height="20" class="block mx-auto" /> Free Download
-                    </div>
+                    </button>
                 </a>
               </div>
               {{-- ad --}}
@@ -270,6 +268,45 @@
          </div>
         {{-- more images end --}}
         </div>
+         {{-- Download Modal Start --}}
+         <div id="download-modal" data-modal-placement="center-center" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+            <div class="relative w-full h-full max-w-md md:h-auto">
+                <!-- Modal content -->
+                <div class="relative bg-slate-800 rounded-lg shadow dark:bg-gray-700">
+                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-slate-600 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="download-modal">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                    <div class="px-6 py-6 lg:px-8">
+                        <h3 class="mb-4 text-xl text-gray-300 font-bold dark:text-white">Select Image Quality</h3>
+                        <form class="space-y-6" action="#">
+                        <ul class="w-full text-sm font-medium text-gray-900 border border-gray-700 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            <li class="w-full border-b border-gray-700 rounded-t-lg dark:border-gray-600">
+                                <div class="flex items-center pl-3">
+                                    <input id="list-radio-license" type="radio" value="low" name="imageQuality" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                    <label for="list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-gray-200 dark:text-gray-300">Low Quality</label>
+                                </div>
+                            </li>
+                            <li class="w-full border-b border-gray-700 rounded-t-lg dark:border-gray-600">
+                                <div class="flex items-center pl-3">
+                                    <input id="list-radio-millitary" type="radio" value="medium" name="imageQuality" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                    <label for="list-radio-millitary" class="w-full py-3 ml-2 text-sm font-medium text-gray-200 dark:text-gray-300">Medium Quality</label>
+                                </div>
+                            </li>
+                            <li class="w-full border-b border-gray-700 rounded-t-lg dark:border-gray-600">
+                                <div class="flex items-center pl-3">
+                                    <input id="list-radio-passport" type="radio" value="high" name="imageQuality" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                    <label for="list-radio-passport" class="w-full py-3 ml-2 text-sm font-medium text-gray-200 dark:text-gray-300">High Quality <span class=" text-xs text-gray-400 font-semibold">( Login required )</span></label>
+                                </div>
+                            </li>
+                        </ul>
+                        <button type="submit" class=" bg-slate-900 w-full py-3 rounded-full text-gray-300 font-bold">Download</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+         {{-- Download Modal End --}}
     </div>
     </div>
 </div>
