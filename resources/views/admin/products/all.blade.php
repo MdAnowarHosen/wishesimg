b1da00da89c4a65b60a4615f952291c88ff56c13
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-04-10 18:20:15 +0600

    svg bug fix

--- resources/views/admin/products/all.blade.php
+++ resources/views/admin/products/all.blade.php
@@ -9,14 +9,14 @@
         @endcell
 
         @cell('action', $product)
-        <Link href="{{ route('admin.products.show',$product->id) }}"><x-bi-eye-fill class="w-5 h-5 inline mx-2" /></Link>
-        <Link href="{{ route('admin.products.edit',$product->id) }}"><x-fas-edit class="w-5 h-5 inline mx-2" /></Link>
+        <Link href="{{ route('admin.products.show',$product->id) }}"><x-tabler-eye class="w-6 h-6 inline mx-2" /></Link>
+        <Link href="{{ route('admin.products.edit',$product->id) }}"><x-tabler-edit class="w-6 h-6 inline mx-2" /></Link>
         <Link href="{{ route('admin.products.destroy',$product->id) }}"
         method="delete"
         confirm="Delete"
         confirm-text="Are you sure you want to delete?"
         confirm-button="Yes, Delete!"
-        cancel-button="No, keep me save!"><x-bi-trash3-fill class="w-5 h-5 inline mx-2" /></Link>
+        cancel-button="No, keep me save!"><x-tabler-trash class="w-6 h-6 inline mx-2" /></Link>
         @endcell
     </x-splade-table>
 </section>
--- resources/views/admin/sub-categories/all.blade.php
+++ resources/views/admin/sub-categories/all.blade.php
@@ -1,13 +1,13 @@
 <section>
     <x-splade-table :for="$subCategories">
         @cell('action', $subCategory)
-        <Link href="{{ route('admin.sub-category.edit',$subCategory->id) }}"><x-fas-edit class="w-5 h-5 inline mx-2" /></Link>
+        <Link href="{{ route('admin.sub-category.edit',$subCategory->id) }}"><x-tabler-eye class="w-6 h-6 inline mx-2" /></Link>
         <Link href="{{ route('admin.sub-category.destroy',$subCategory->id) }}"
         method="delete"
         confirm="Delete"
         confirm-text="Are you sure you want to delete?"
         confirm-button="Yes, Delete!"
-        cancel-button="No, keep me save!"><x-bi-trash3-fill class="w-5 h-5 inline mx-2" /></Link>
+        cancel-button="No, keep me save!"><x-tabler-trash class="w-6 h-6 inline mx-2" /></Link>
         @endcell
     </x-splade-table>
 </section>
