c90351915cd5aaea7ee81e600aad4be3401227e0
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-04-14 10:10:32 +0600

    google search console verification js ad

--- resources/views/layouts/frontendLayout.blade.php
+++ resources/views/layouts/frontendLayout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google-site-verification" content="gGvxel-XKsdNkFgH6gijPGsFv5zuY9hu9gAPxRmjVaE" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
     @spladeHead
     @vite('resources/js/app.js')
