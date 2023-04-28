9bafed943cb09fe1e7ff21cc07d5ea48b4150239
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-04-28 14:54:42 +0600

    sidebar folder color

--- resources/views/layouts/frontendLayout.blade.php
+++ resources/views/layouts/frontendLayout.blade.php
@@ -123,21 +123,19 @@
               </div>
             </div>
           </nav>
-
           <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
              <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
                 <ul class="space-y-2">
                    <li>
                       <a href="{{ route('/') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <x-entypo-home class="w-5 h-5 text-gray-600 inline" /> <span class="ml-3 inline">Home</span>
+                        <x-entypo-home class="w-5 h-5 text-amber-600 inline" /> <span class="ml-3 inline">Home</span>
                       </a>
                    </li>
-
                    @foreach ($boot_categories as $category)
                    @if ($category->activatedSubCatsAsc != null && isset($category->activatedSubCatsAsc) && count($category->activatedSubCatsAsc)>0)
                    <li>
                     <button type="button" aria-controls="{{ $category->slug }}" data-collapse-toggle="{{ $category->slug }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                        <x-bi-folder class="w-5 h-5"/>
+                        <x-bi-folder-fill class="w-5 h-5 text-amber-500"/>
                           <span class="flex-1 ml-1 text-left whitespace-nowrap" sidebar-toggle-item>{{ $category->name }}</span>
                           <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
@@ -145,22 +143,27 @@
                     <ul id="{{ $category->slug }}" class="hidden py-2 space-y-1">
                           @foreach ($category->activatedSubCatsAsc as $subCat)
                           <li>
-                             <a href="{{ route('images.subcat.products',['mainCatSlug'=>$category->slug, 'subCategorySlug'=>$subCat->slug]) }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-5 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"> <x-bi-folder2-open class="w-5 h-5 mr-2"/> {{ $subCat->name }}</a>
+                             <a href="{{ route('images.subcat.products',['mainCatSlug'=>$category->slug, 'subCategorySlug'=>$subCat->slug]) }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-5 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"> <x-bi-folder-fill  class="w-5 h-5 mr-2 text-amber-400"/> {{ $subCat->name }}</a>
                           </li>
                          @endforeach
-
                     </ul>
                  </li>
                  @endif
                  @endforeach
                  <li>
-                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <x-eos-privacy-tip-o class="w-5 h-5 text-gray-600"/>
+                    <a href="{{ route('sitemap') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <x-tabler-sitemap class="w-5 h-5 text-gray-600"/>
+                       <span class="flex-1 ml-3 whitespace-nowrap">Sitemap</span>
+                    </a>
+                 </li>
+                 <li>
+                    <a href="{{ url('privacy-policy') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <x-eos-privacy-tip-o class="w-7 h-5 text-gray-600"/>
                        <span class="flex-1 ml-3 whitespace-nowrap">Privacy Policy</span>
                     </a>
                  </li>
                  <li>
-                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <a href="{{ route('about') }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                         <x-bi-info-circle class="w-5 h-5"/>
                        <span class="flex-1 ml-3 whitespace-nowrap">About</span>
                     </a>
